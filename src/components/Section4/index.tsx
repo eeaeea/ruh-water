3689b1cc100d679c0b84c9396f87c84b1b3fc692
Elvis <elvis.e.amancio@gmail.com>
2021-06-03 20:36:43 -0300

    + Changes

--- src/components/Section4/index.tsx
+++ src/components/Section4/index.tsx
@@ -4,7 +4,17 @@ export default function Section4() {
   return (
     <>
       <div className={styles.container}>
-        
+        <div className={styles.containerContent}>
+          <div className={styles.containerHeader}>
+            <span>BRANDS</span>
+            <h1>We work with thousands of the world’s<br /> leading brands</h1>
+          </div>
+          <div className={styles.containerCompanys}></div>
+          <span>
+            You're going to be in good company. We work with<br /> world's leading
+            brands.
+          </span>
+        </div>
       </div>
     </>
   );
