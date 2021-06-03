e94d737f1dcea9a1c6374c5c3d3b16fe3001c0df
Elvis <elvis.e.amancio@gmail.com>
2021-06-03 18:05:03 -0300

    first commit

---
+++ src/components/SectionOne/index.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export default function SectionOne() {
+  return (
+    <>
+      <div className={styles.container}>
+        <header>
+          <Image
+            src="/images/logo.png"
+            alt="Logo Empresa"
+            height={100}
+            width={100}
+          />
+          <nav>
+            <a href="http://">Home</a>
+            <a href="http://">Seus benefícios</a>
+            <a href="http://">Nosso processo</a>
+            <a href="http://">Nossos trabalhos</a>
+            <a href="http://">Testemunhos</a>
+            <a href="http://">Inicie um projeto</a>
+          </nav>
+          <button>GET STARTED</button>
+        </header>
+
+        <div className={styles.containerContent}>
+          <h1>Uma agência de design de produto digital</h1>
+          <h2>Somos Agência Digital Criativa e Profissional</h2>
+          <button>GET STARTED</button>
+          <Image src="/images/hero.png" alt="" height={786} width={1080} />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import SectionOne from "../components/SectionOne";
+import SectionTwo from "../components/SectionTwo";
+import SectionThree from "../components/SectionThree";
+
+export default function App() {
+  return (
+    <>
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+    </>
+  );
+}
