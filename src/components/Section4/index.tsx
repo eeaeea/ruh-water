9f49926c0ad855efcaa3bdfd87e946a6cee9b511
Elvis <elvis.e.amancio@gmail.com>
2021-06-05 14:18:29 -0300

    Ajustes

--- src/components/Section4/index.tsx
+++ src/components/Section4/index.tsx
@@ -4,26 +4,23 @@ export default function Section4() {
   return (
     <>
       <div id="section4" className={styles.container}>
+        <span>PARCEIROS</span>
+        <h1>
+          Empresas parceiras
+          <br /> da RuhWather
+        </h1>
+
         <div className={styles.containerContent}>
-          <div className={styles.containerHeader}>
-            <span>PARCEIROS</span>
-            <h1>
-              Empresas parceiras
-              <br /> da RuhWather
-            </h1>
-          </div>
-          <div className={styles.containerCompanys}>
-            <img src="/images/parceiro-1.png" alt="" />
-            <img src="/images/parceiro-2.png" alt="" />
-            <img src="/images/parceiro-6.png" alt="" />
-            <img src="/images/parceiro-3.png" alt="" />
-          </div>
-          <span>
-            Você estará em boa companhia. Trabalhamos com
-            <br />
-            PARCEIROS líderes mundiais.
-          </span>
+          <img src="/images/parceiro-6.png" alt="" />
+          <img src="/images/parceiro-1.png" alt="" />
+          <img src="/images/parceiro-2.png" alt="" />
+          <img src="/images/parceiro-3.png" alt="" />
         </div>
+
+        <p>
+          Você estará em boa companhia.
+          <br /> Trabalhamos com Parceiros líderes mundiais.
+        </p>
       </div>
     </>
   );
