e147066775d6935a1d5978097042f6d2fea37d75
Elvis <elvis.e.amancio@gmail.com>
2021-06-08 16:09:26 -0300

    Ajustes

--- src/components/Section3/index.tsx
+++ src/components/Section3/index.tsx
@@ -27,6 +27,13 @@ export default function Section3() {
               fluviométricos, gerenciamento do fluxo e vazão para auxílio em
               decisões.
             </p>
+
+            <ul>
+              <li>- Medição da superfície do corpo d’água</li>
+              <li>- Vasão residual de barragens</li>
+              <li>- Dispensa sinal de rede e internet</li>
+              <li>- Garantia de 12 meses</li>
+            </ul>
           </div>
         </div>
 
@@ -44,6 +51,14 @@ export default function Section3() {
               Solução inteligente para contabilização de tempo de funcionamento
               de dispositivos, gerenciamento de vazão volumétrica e relatórios.
             </p>
+
+            <ul>
+              <li>- Dados de vazão volumétrica</li>
+              <li>- Relatório de horas trabalhadas</li>
+              <li>- Lâmpada indicativa de função</li>
+              <li>- Funciona através de contato seco</li>
+              <li>- Garantia de 12 meses</li>
+            </ul>
           </div>
         </div>
 
@@ -61,6 +76,14 @@ export default function Section3() {
               Solução inteligente para medição automática do volume de chuvas,
               gerenciamento da quantidade de chuva com relatórios e gráficos.
             </p>
+
+            <ul>
+              <li>- Medição do volume de chuvas</li>
+              <li>- Coleta automática</li>
+              <li>- Previsão de chuvas</li>
+              <li>- Auxílio para tomada de decisões</li>
+              <li>- Garantia de 12 meses</li>
+            </ul>
           </div>
         </div>
       </div>
